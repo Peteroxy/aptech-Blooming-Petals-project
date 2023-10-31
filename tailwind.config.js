c40580a36d3f7d2d21d72128489ce374b42800b7
adunmoye peter <adunmoyepeter111@gmail.com>
2023-10-31 10:09:00 +0000

    my commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'tangerine': ['Tangerine', 'cursive'],
        
       },
+      animation: {
+        // Bounces 5 times 1s equals 5 seconds
+        'spin-short': 'spin 1s ease-in-out 1'
+      },
     },
   },
   plugins: [],
